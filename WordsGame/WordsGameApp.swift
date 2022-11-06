4f0904b15455548ff44d8d5c6f63196c2010ef68
raster-gen <rasterroot@gmail.com>
2022-11-06 14:24:56 +0000

    Initial Commit

---
+++ WordsGame/WordsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsGameApp.swift
+//  WordsGame
+//
+//  Created by Gennady Raster on 6.11.22.
+//
+
+import SwiftUI
+
+@main
+struct WordsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
